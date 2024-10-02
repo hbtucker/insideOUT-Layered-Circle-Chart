87d70b48c868af8c15a5e028cd1adee82183ea1c
Culture-Coach <132301974+hbtucker@users.noreply.github.com>
2024-10-02 16:17:36 +1300

    Update layered_circles_chart.js

--- layered_circles_chart.js
+++ layered_circles_chart.js
@@ -61,7 +61,7 @@ function _chart(d3,data)
   const label = svg
     .append("g")
     .style("font-family", "'Poppins', sans-serif")
-    .style("font-size", "15px")
+    .style("font-size", "14px")
     .attr("pointer-events", "none")
     .attr("text-anchor", "middle")
     .selectAll("text")
